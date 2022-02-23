fd83267171b95d874c58933ff5b1c6a232e49d16
Leonid Makavchuk <makavchuk@gmail.com>
2022-02-23 16:05:53 +0300

    First implementation

--- src/main/java/web/controller/MainController.java
+++ src/main/java/web/controller/MainController.java
@@ -2,45 +2,67 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import web.model.CarSupplier;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import web.model.User;
+import web.service.UserService;
 
 @Controller
+
 public class MainController {
 
 	@Autowired
-	public CarSupplier carSupplier;
+	UserService userService;
 
-	@GetMapping(value = "/")
-	public String printWelcome(ModelMap model) {
-		List<String> messages = new ArrayList<>();
-		messages.add("Hello!");
-		messages.add("I'm Spring MVC application");
-		messages.add("5.2.0 version by sep'19 ");
-		model.addAttribute("messages", messages);
-		return "index";
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView mainPage() {
+		ModelAndView modelAndView = new ModelAndView("list");
+		modelAndView.getModel().put("users", userService.getUsers());
+		return modelAndView;
 	}
 
-	@GetMapping(value = "/cars")
-	public String printCars(ModelMap model, @RequestParam(value = "count", required = false) Integer count) {
-
-		if (count == null || count < 0 || count > carSupplier.carsStorage.size()) {
-			model.addAttribute("cars", carSupplier.carsStorage);
-		} else {
-			model.addAttribute("cars", carSupplier.carsStorage.subList(0, count));
-		}
-
-		model.addAttribute("cars_supplier_address", carSupplier.hashCode());
-		model.addAttribute("cars_requested", count);
-		model.addAttribute("cars_available", carSupplier.carsStorage.size());
-
-		return "cars";
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public ModelAndView addPage() {
+		return addEditPage(-1, "/add");
 	}
+
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public ModelAndView editPage(@PathVariable int id) {
+		return addEditPage(id, "/edit/" + id);
+	}
+
+	public ModelAndView addEditPage(int id, String action) {
+		ModelAndView modelAndView = new ModelAndView("form");
+		User user = id < 0 ? new User() : userService.getUser(id);
+		modelAndView.getModel().put("user", user);
+		modelAndView.getModel().put("action", action);
+		return modelAndView;
+	}
+
+	//////////////////
+
+	@RequestMapping(value = "/add", method = {RequestMethod.PUT, RequestMethod.POST})
+	public ModelAndView createUser(@ModelAttribute("user") User user) {
+		System.out.println("inserting " + user);
+		userService.addUser(user);
+		return new ModelAndView("redirect:/");
+	}
+
+	@RequestMapping(value = "/edit/{id}", method = {RequestMethod.PUT, RequestMethod.POST})
+	public ModelAndView updateUser(@ModelAttribute("user") User user, @PathVariable int id) {
+		System.out.println("updating id = " + id);
+		user.setId(id);
+		userService.updateUser(user);
+		return new ModelAndView("redirect:/");
+	}
+
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public ModelAndView deleteUser(@PathVariable int id) {
+		System.out.println("deleting id = " + id);
+		userService.deleteUser(id);
+		return new ModelAndView("redirect:/");
+	}
+
 }
 
 
